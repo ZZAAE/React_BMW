e12490d214bc61b294828dcb77dfc3651c90f956
unsle <nayyeon.dgu@gmail.com>
2026-03-15 00:45:05 +0900

    오탈자

--- src/components/ReviewList.jsx
+++ src/components/ReviewList.jsx
@@ -9,8 +9,13 @@ const ReviewList = ({ data, className }) => {
             {data.map((item) => (
                 <button key={item.id} className="review-list-item" onClick={() => navigate(`/review/${item.id}`)}>
                     <img src={item.media_info.thumbnail} alt={item.media_info.title} />
-                    <h3>{item.media_info.title}</h3>
-                    <span>{item.media_info.creator}</span>
+
+                    {className === "scroll-layout" && (
+                        <>
+                        <h3>{item.media_info.title}</h3>
+                        <span>{item.media_info.genre}</span>
+                        </>
+                    )}
                 </button>
             ))}
         </div>
